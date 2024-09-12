a7ac3a5403e0c5a5da88a4cbfba862bb7f5b7e52
Ayush Singh <asayushsingh80@gmail.com>
2024-09-12 23:06:30 +0530

    ui: add overlay

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Paginator, PaginatorPageChangeEvent } from "primereact/paginator";
+import { OverlayPanel } from "primereact/overlaypanel";
 
 type ColumnType = {
   field: string;
@@ -25,6 +26,7 @@ function App() {
   const [selectedArtworks, setSelectedArtworks] = useState([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [first, setFirst] = useState<number>(0);
+  const [inputValue, setInputValue] = useState<number>(0);
   const [totalRecords, setTotalRecords] = useState<number | undefined>(
     undefined
   );
@@ -50,38 +52,68 @@ function App() {
     fetchDataByPage(event.page + 1);
   };
 
+  const op = useRef<OverlayPanel>(null);
+
+  const handleOverlay = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (op.current) {
+      op.current.toggle(event);
+    }
+  };
+
   useEffect(() => {
     fetchDataByPage(1);
   }, [fetchDataByPage]);
 
   return (
-    <div className="container">
-      <DataTable
-        loading={isLoading}
-        selectionMode={"checkbox"}
-        first={2}
-        rows={12}
-        selection={selectedArtworks}
-        onSelectionChange={(e) => setSelectedArtworks(e.value)}
-        dataKey="id"
-        value={artworks}
-        tableStyle={{ minWidth: "50rem" }}
-      >
-        <Column
-          selectionMode="multiple"
-          headerStyle={{ width: "3rem" }}
-        ></Column>
-        {columns.map((col) => (
-          <Column key={col.field} field={col.field} header={col.header} />
-        ))}
-      </DataTable>
-      <Paginator
-        first={first}
-        rows={12}
-        totalRecords={totalRecords}
-        onPageChange={onPageChange}
-      />
-    </div>
+    <>
+      <div className="container">
+        <DataTable
+          loading={isLoading}
+          selectionMode={"checkbox"}
+          first={2}
+          rows={12}
+          selection={selectedArtworks}
+          onSelectionChange={(e) => setSelectedArtworks(e.value)}
+          dataKey="id"
+          value={artworks}
+          tableStyle={{ minWidth: "50rem" }}
+        >
+          <Column
+            selectionMode="multiple"
+            headerStyle={{ width: "3rem" }}
+          ></Column>
+          <Column
+            headerStyle={{ width: "3rem" }}
+            header={
+              <button className="chevron-dropdown" onClick={handleOverlay}>
+                <img className="chevron" src="/chevron-down.svg" />
+              </button>
+            }
+          ></Column>
+          {columns.map((col) => (
+            <Column key={col.field} field={col.field} header={col.header} />
+          ))}
+        </DataTable>
+        <Paginator
+          first={first}
+          rows={12}
+          totalRecords={totalRecords}
+          onPageChange={onPageChange}
+        />
+      </div>
+      <OverlayPanel ref={op}>
+        <div className="overlaypanel-container">
+          <input
+            type="number"
+            className="overlaypanel-input"
+            placeholder="Enter Number of rows"
+            value={inputValue}
+            onChange={(e) => setInputValue(Number(e.currentTarget.value))}
+          />
+          <button className="overlaypanel-button">Submit</button>
+        </div>
+      </OverlayPanel>
+    </>
   );
 }
 
