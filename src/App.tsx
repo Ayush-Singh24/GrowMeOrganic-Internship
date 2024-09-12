8158515951b5e08ec353b8779fdfe95f92f8ce41
Ayush Singh <asayushsingh80@gmail.com>
2024-09-12 19:11:15 +0530

    feat: fetch data and render table

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import "./App.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
-import "primeicons/primeicons.css"; //icons
-import "primeflex/primeflex.css"; // flex
 import "./App.css";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { useEffect, useState } from "react";
+
+type ColumnType = {
+  field: string;
+  header: string;
+};
+
+const columns: ColumnType[] = [
+  { field: "title", header: "Title" },
+  { field: "place_of_origin", header: "Origin" },
+  { field: "artist_display", header: "Artist" },
+  { field: "inscriptions", header: "Inscriptions" },
+  { field: "date_start", header: "Start" },
+  { field: "date_end", header: "End" },
+];
 
 function App() {
-  return <div className="container">hello</div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://api.artic.edu/api/v1/artworks?page=1").then((res) =>
+      res.json().then((data) => {
+        setProducts(data.data);
+      })
+    );
+  }, []);
+  return (
+    <div className="container">
+      <DataTable value={products} tableStyle={{ minWidth: "50rem" }}>
+        {columns.map((col) => (
+          <Column key={col.field} field={col.field} header={col.header} />
+        ))}
+      </DataTable>
+    </div>
+  );
 }
 
 export default App;
